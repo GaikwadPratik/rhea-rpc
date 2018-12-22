b299f195f4756c3c43011030f835d9360a45c07f
Pratik Gaikwad <pratik.gaikwad19@gmail.com>
2018-12-22 09:03:46 -0500

    refactor(typings): Corrected typings for library

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,32 @@
 import { Connection, ConnectionOptions } from "rhea-promise";
-import { RpcClient } from "./lib/rpcClient";
-import { RpcServer } from "./lib/rpcServer";
-import { MessageOptions } from "./lib/util/common";
+
+export declare interface ServerFunctionDefinition {
+    name: string,
+    params?: {
+        type: string,
+        properties: any
+    }
+}
+
+export declare class RpcServer {
+    constructor(amqpNode: string, connection: Connection);
+    bind(functionDefintion: ServerFunctionDefinition, callback: Function);
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+}
+
+export declare class RpcClient {
+    constructor(amqpNode:string, connection: Connection, options?: MessageOptions);
+    call(functionName: string, ...args: Array<any>): Promise<{}>;
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+}
+
+export declare interface MessageOptions {
+    timeout: number
+}
+
 export declare class RheaRpc {
-    readonly Connection: Connection;
     createAmqpClient(connectionOptions: ConnectionOptions): Promise<RheaRpc>;
     createRpcClient(amqpNode: string, options?: MessageOptions): Promise<RpcClient>;
     createRpcServer(amqpNode: string): Promise<RpcServer>;
