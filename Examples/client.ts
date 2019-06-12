6161ad2837d86f28c619b6ccd49bc7000a785b02
Pratik Gaikwad <pratik.gaikwad19@gmail.com>
2019-06-12 13:39:58 -0400

    refactor(client): close the client explicitely

--- Examples/client.ts
+++ Examples/client.ts
@@ -8,15 +8,26 @@ async function Main() {
         password: ''
     };
     const _client = await new RheaRpc().createAmqpClient(_connectionOptions);
-    const _rpcClient  = await _client.createRpcClient('amq.topic');
-    console.log(await _rpcClient.call('namedParams', { firstName: '123', lastName: '456'}));
-    console.log(await _rpcClient.call('simpleParams', ['123', '456']));
-    console.log(await _rpcClient.call('noParams'));
+    let _rpcClient = await _client.createRpcClient('amq.topic');
+    console.log(await _rpcClient.call('namedParams', { firstName: '123', lastName: '456' })
+        .finally(() => _rpcClient.disconnect()));
+    _rpcClient = await _client.createRpcClient('amq.topic');
+    console.log(await _rpcClient.call('simpleParams', ['123', '456'])
+        .finally(() => _rpcClient.disconnect()));
+    _rpcClient = await _client.createRpcClient('amq.topic');
+    console.log(await _rpcClient.call('noParams')
+        .finally(() => _rpcClient.disconnect()));
 
-    const _rpcClientWithSubject  = await _client.createRpcClient('amq.topic/test');
-    console.log(await _rpcClientWithSubject.call('namedParams', { firstName: '123', lastName: '456'}));
-    console.log(await _rpcClientWithSubject.call('simpleParams', ['123', '456']));
-    console.log(await _rpcClientWithSubject.call('noParams'));
+    let _rpcClientWithSubject = await _client.createRpcClient('amq.topic/test');
+    console.log(await _rpcClientWithSubject.call('namedParams', { firstName: '123', lastName: '456' })
+        .finally(() => _rpcClientWithSubject.disconnect()));
+    _rpcClientWithSubject = await _client.createRpcClient('amq.topic/test');
+    console.log(await _rpcClientWithSubject.call('simpleParams', ['123', '456'])
+        .finally(() => _rpcClientWithSubject.disconnect()));
+    _rpcClientWithSubject = await _client.createRpcClient('amq.topic/test');
+    console.log(await _rpcClientWithSubject.call('noParams')
+        .finally(() => _rpcClientWithSubject.disconnect()));
+
 }
 
 Main()
